1a30d8dd62bd7d6f816bccf796296470af90b530
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-02-13 12:06:39 +0530

    Time: 3 ms (69.85%), Space: 7.6 MB (42.48%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        if(nums.empty()) return {{}}; //if empty return {}
-        vector<vector<int>> result={{}}; //result for all subset
-        for(int x:nums)
+    void subsets(vector<int>nums,int idx,vector<vector<int>>& result)
+    {
+        if(idx==nums.size()-1) //if size is one-only one element
         {
+            result.push_back({}); //push empty set 
+            result.push_back({nums[idx]}); //push set itself
+        }
+        else
+        {
+            subsets(nums,idx+1,result); //call function
             int n=result.size();
             for(int i=0;i<n;i++)
             {
-               vector<int> r=result[i]; //store current subset
-                r.push_back(x);
-                result.push_back(r);
+                vector<int>r=result[i];
+                r.push_back(nums[idx]); //r is current subset
+                result.push_back(r);  //push r into result
             }
         }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        if (nums.empty()) return {{}};
+        vector<vector<int>>  result;
+        subsets(nums,0,result); //recursive function
         return result;
     }
 };
