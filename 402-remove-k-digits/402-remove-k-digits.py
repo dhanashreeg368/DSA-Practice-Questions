b3795f78c5aa642da9d837708c686ed3609d53fe
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-02-18 10:54:24 +0530

    Time: 1998 ms (5.07%), Space: 14.1 MB (93.92%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -3,16 +3,16 @@ class Solution:
         li_num=list(num)  #converting to list    
         while(k>0):
             i=0
-            found=0 #flag to check if current is greater than (0: not greater)
+            found=0 #flag to check if current is greater than next(0: not greater)
             while(i<len(li_num)-1 and k>0):
-                if int(li_num[i])>int(li_num[i+1]): #if current>next: del current
+                if int(li_num[i])>int(li_num[i+1]): #if current>next:del current
                     del li_num[i]
                     found=1 #(flag set to 1 means greater current is found)
                     k=k-1 #decrement k
                     break #iterate from start again
                 i+=1
 
-            if found==0: #if no greater is found eg.1234 (then remove last ele)
+            if found==0: #if no greater is found eg.1234 (then remove last k elements)
                 while(k>0):          
                     li_num.pop()
                     k=k-1
