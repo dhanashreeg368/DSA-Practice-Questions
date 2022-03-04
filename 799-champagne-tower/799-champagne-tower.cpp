628668bed9848d2ecffe7203adf38b34f61d3204
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-03-04 20:16:45 +0530

    Time: 7 ms (77.72%), Space: 6 MB (91.14%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[101][101]={0.0}; //amt of champagne to be poured 
+        dp[0][0]=poured; //base case
+        for (int i=0;i<100;i++)
+        {
+            for(int j=0;j<=i;j++) 
+            {
+              if (dp[i][j]>=1)
+              {
+                  dp[i+1][j]+=(dp[i][j]-1)/2.0; //left
+                  dp[i+1][j+1]+= (dp[i][j]-1)/2.0; //right
+                  dp[i][j]=1;
+              }
+            }
+        }
+        return dp[query_row][query_glass];
+    }
+};
