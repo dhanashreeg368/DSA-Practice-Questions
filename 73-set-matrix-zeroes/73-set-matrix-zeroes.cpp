a7c0fdfeaf2a1e7b3792c199749ee0c42c232ebc
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-06-16 11:34:06 +0530

    Time: 17 ms (71.14%), Space: 13.2 MB (81.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i][0] == 0) col0 = 0;
-        for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
-    }
-
-    for (int i = rows - 1; i >= 0; i--) {
-        for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
-        if (col0 == 0) matrix[i][0] = 0;
-    }
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        vector<int> dummy1(rows,-1); //dummy for rows 
+        vector<int> dummy2(cols,-1); //dummy for cols
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if (matrix[i][j]==0)
+                {
+                    dummy1[i]=0; //if zero update in rows and cols
+                    dummy2[j]=0;
+                }
+            }
+        }
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++) //traverse again 
+            {
+                if (dummy1[i]==0 || dummy2[j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
     }
 };
