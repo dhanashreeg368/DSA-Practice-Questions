92b310bd81a0677fd0bc381c2793ec55a9a80491
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-07-23 23:39:51 +0530

    Time: 3380 ms (71.88%), Space: 30 MB (98.83%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,13 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+        sortedList = SortedList()
+        for i in range(n - 1, -1, -1):
+            index = sortedList.bisect_left(nums[i])
+            ans[i] = index
+            sortedList.add(nums[i])
+        return ans
+        
