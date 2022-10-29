492a15a8f9d7d18d2d16d228c34f2e06625ff99a
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2022-10-29 07:44:27 +0530

    Added solution - LeetHub

---
+++ Pattern 3 - GFG/pattern-3.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++) { //for total number of rows
+            for(int j=1; j<=i;j++) { //for innner values of rows
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
