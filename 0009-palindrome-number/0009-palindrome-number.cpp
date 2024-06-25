00f9393c9975633af2314b8ce45d38cab04d0605
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2024-06-25 11:37:30 +0530

    Time: 8 ms (59.96%), Space: 8.3 MB (29.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) {
+            return false;
+        }
+        long long reverse=0;
+        int temp=x;
+        while(temp!=0) {
+            int digit = temp%10;
+            reverse=reverse*10 + digit;
+            temp/=10;
+        }
+        return reverse==x;
+    }
+};
