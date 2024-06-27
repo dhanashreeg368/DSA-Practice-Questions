dc733070d1733ae0d6655e3ef3907735b130011f
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2024-06-27 11:55:54 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (86.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++) {
+            char ch = s[i];
+            if (ch=='(' || ch=='[' || ch=='{') {
+                st.push(ch);
+            }
+            else {
+                if (!st.empty()) {
+                    char top = st.top();
+                    if ((ch==')' and top=='(') || 
+                        (ch==']' and top=='[') || 
+                        (ch=='}' and top=='{')) {
+                        st.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }       
+        }
+        if (st.empty()) {
+                return true;
+        } else {
+            return false;
+        }
+    }
+};
