c4202bcf7bdc2511ae11dfe8e8e4b8644d9792b2
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2024-06-27 13:07:27 +0530

    Time: 4 ms (46.43%), Space: 7.8 MB (84.20%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> mp;
-        int length=0;
-        int oddCountFound=false;
-        for (int i=0; i<s.length(); i++) {
-            char ch=s[i];
-            mp[ch]++;
+        vector<int> count(128, 0); // Assuming ASCII characters
+        
+        for (char ch : s) {
+            count[ch]++;
         }
-        for (auto x: mp) {
-            if (x.second%2==0) {
-                length+=x.second;
+        
+        int length = 0;
+        bool oddCountFound = false;
+        
+        for (int c : count) {
+            if (c % 2 == 0) {
+                length += c;
+            } else {
+                length += c - 1;
+                oddCountFound = true;
             }
-            else {
-                oddCountFound=true;
-                length+=x.second-1;
-            }
         }
-        if (oddCountFound==true) {
-            length+=1;
+        
+        if (oddCountFound) {
+            length++;
         }
+        
         return length;
+
     }
 };
