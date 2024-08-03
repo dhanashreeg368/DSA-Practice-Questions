28a90604b1ea722eb516f6af353575b93426707d
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2024-08-03 17:38:34 +0530

    Time: 35 ms (84.34%), Space: 58.1 MB (25.07%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) { // longest subarray with atmost k zeroes 
+        int n = nums.size();
+        int maxLen = 0;
+        int l=0, r=0;
+        int zeroes = 0; // count zeroes 
+        while(r<n) {
+            if (nums[r] == 0) { // if zero encountered, incr count
+                zeroes++;
+            }
+            while (zeroes > k) { // if not valid condtion, left++, and if 0 encountered, decr it from cnt
+                if (nums[l] == 0) {
+                    zeroes--;
+                }
+                l++;
+            }
+            if (zeroes <=k) { // if valid condition --> caluclate max length
+                maxLen = max(maxLen, r-l+1);
+            }
+            r+=1;
+        }
+        return maxLen;
+    }
+};
