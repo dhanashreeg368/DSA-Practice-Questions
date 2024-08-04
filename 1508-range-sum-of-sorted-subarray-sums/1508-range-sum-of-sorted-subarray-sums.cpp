5066c8cc6dcb1ebd9b5967f72b41316ac6779452
Dhanashree Gaikwad <68561090+dhanashreeg368@users.noreply.github.com>
2024-08-04 11:33:54 +0530

    Time: 98 ms (28.31%), Space: 27.3 MB (19.10%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -3,15 +3,21 @@ public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         int mod = 1e9+7;
         vector<int> res;
+        vector<int> prefixSums(n+1, 0);
+        
+        // compute prefix sums
+        for(int i=0; i<n; i++) {
+            prefixSums[i+1] = (prefixSums[i] + nums[i]) % mod;
+        }
         
         // generate all subarray sums
         for(int i=0; i<n; i++) {
             int sum = 0;
-            for(int j=i; j<n; j++) {
-                sum = (sum + nums[j]) % mod;
-                res.push_back(sum);
+            for(int j=i+1; j<=n; j++) {
+                res.push_back((prefixSums[j] - prefixSums[i] + mod) % mod);
             }
         }
+        
         // sort
         sort(res.begin(), res.end());
         
